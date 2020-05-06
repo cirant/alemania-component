47d12858f1daed126b5dced99927f7d436454384
liliana colls <liliana.colls@2brains.cl>
2020-05-06 09:11:44 -0400

    crea componentes

--- src/App.vue
+++ src/App.vue
@@ -1,213 +1,233 @@
 <template>
   <div id="app">
-    <Card align="left">
-      <div>
-        <Typography variant="h4" weight="bold" type="primary">DIAGNÓSTICO</Typography>
-        <Typography variant="span" size="12">Última actualización: 10 / 07 / 2017, 20:30 hrs.</Typography>
-      </div>
-      <Table>
-        <thead>
-          <tr>
-            <th>
-              <input type="checkbox" />
-            </th>
-            <th>nombre del diagnóstico</th>
-            <th>estado</th>
-            <th>fecha</th>
-            <td colspan="2"></td>
-            <th>acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td align="center">
-              <input type="checkbox" />
-            </td>
-            <td>
-              <Typography
-                variant="p"
-                type="primary"
-              >Afecciones respiratorias debidas a inhalación de gases, humos…</Typography>
-            </td>
-            <td>
-              <Status value="confirmado" />
-            </td>
-            <td>
-              <Typography>2014-12-24 24:15:00</Typography>
-            </td>
-            <td>
-              <Typography weight="bold">ENO</Typography>
-            </td>
-            <td>
-              <Typography weight="bold">GES</Typography>
-            </td>
-            <td>
-              <div class="actions-container">
-                <Buttom variant="text" icon="edit">Editar</Buttom>
-                <Buttom variant="text" color="error" icon="trash">Eliminar</Buttom>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </Table>
-      <table class="table">
-        <thead>
-          <tr>
-            <th>
-              <input type="checkbox" />
-            </th>
-            <th>nombre del diagnóstico</th>
-            <th>estado</th>
-            <th>fecha</th>
-            <th>acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <input type="checkbox" />
-            </td>
-            <td>
-              <Typography
-                variant="p"
-                type="primary"
-              >Afecciones respiratorias debidas a inhalación de gases, humos…</Typography>
-            </td>
-            <td>
-              <Status value="confirmado" />
-            </td>
-            <td>
-              <Typography>2014-12-24 24:15:00</Typography>
-            </td>
-            <td>
-              <Typography weight="bold">ENO</Typography>
-            </td>
-            <td>
-              <Typography weight="bold">GES</Typography>
-            </td>
-            <td>
-              <div class="actions-container">
-                <Buttom variant="text" icon="edit">Editar</Buttom>
-                <Buttom variant="text" color="error" icon="trash">Eliminar</Buttom>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <input type="checkbox" />
-            </td>
-            <td>
-              <Typography
-                variant="p"
-                type="primary"
-              >Afecciones respiratorias debidas a inhalación de gases, humos…</Typography>
-            </td>
-            <td>
-              <Status value="sospecha" />
-            </td>
-            <td>
-              <Typography>2014-12-24 24:15:00</Typography>
-            </td>
-            <td>
-              <Typography weight="bold">ENO</Typography>
-            </td>
-            <td>
-              <Typography weight="bold">GES</Typography>
-            </td>
-            <td>
-              <div class="actions-container">
-                <Buttom variant="text" icon="edit">Editar</Buttom>
-                <Buttom variant="text" color="error" icon="trash">Eliminar</Buttom>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <input type="checkbox" />
-            </td>
-            <td>
-              <Typography
-                variant="p"
-                type="primary"
-              >Afecciones respiratorias debidas a inhalación de gases, humos…</Typography>
-            </td>
-            <td>
-              <Status />
-            </td>
-            <td>
-              <Typography>2014-12-24 24:15:00</Typography>
-            </td>
-            <td>
-              <Typography weight="bold">ENO</Typography>
-            </td>
-            <td>
-              <Typography weight="bold">GES</Typography>
-            </td>
-            <td>
-              <div class="actions-container">
-                <Buttom variant="text" icon="edit">Editar</Buttom>
-                <Buttom variant="text" color="error" icon="trash">Eliminar</Buttom>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <div>
+      <Typography variant="h4" weight="bold" type="primary">DIAGNÓSTICO</Typography>
+      <Typography variant="span" size="12">Última actualización: 10 / 07 / 2017, 20:30 hrs.</Typography>
+    </div>
+    <Table>
+      <thead>
+        <tr>
+          <th>
+            <input type="checkbox" />
+          </th>
+          <th>nombre del diagnóstico</th>
+          <th>estado</th>
+          <th>fecha</th>
+          <td colspan="2"></td>
+          <th>acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td align="center">
+            <input type="checkbox" />
+          </td>
+          <td>
+            <Typography
+              variant="p"
+              type="primary"
+            >Afecciones respiratorias debidas a inhalación de gases, humos…</Typography>
+          </td>
+          <td>
+            <Status value="confirmado" />
+          </td>
+          <td>
+            <Typography>2014-12-24 24:15:00</Typography>
+          </td>
+          <td>
+            <Typography weight="bold">ENO</Typography>
+          </td>
+          <td>
+            <Typography weight="bold">GES</Typography>
+          </td>
+          <td>
+            <div class="actions-container">
+              <Buttom variant="text" icon="edit">Editar</Buttom>
+              <Buttom variant="text" color="error" icon="trash">Eliminar</Buttom>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </Table>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>
+            <input type="checkbox" />
+          </th>
+          <th>nombre del diagnóstico</th>
+          <th>estado</th>
+          <th>fecha</th>
+          <th>acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <input type="checkbox" />
+          </td>
+          <td>
+            <Typography
+              variant="p"
+              type="primary"
+            >Afecciones respiratorias debidas a inhalación de gases, humos…</Typography>
+          </td>
+          <td>
+            <Status value="confirmado" />
+          </td>
+          <td>
+            <Typography>2014-12-24 24:15:00</Typography>
+          </td>
+          <td>
+            <Typography weight="bold">ENO</Typography>
+          </td>
+          <td>
+            <Typography weight="bold">GES</Typography>
+          </td>
+          <td>
+            <div class="actions-container">
+              <Buttom variant="text" icon="edit">Editar</Buttom>
+              <Buttom variant="text" color="error" icon="trash">Eliminar</Buttom>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="checkbox" />
+          </td>
+          <td>
+            <Typography
+              variant="p"
+              type="primary"
+            >Afecciones respiratorias debidas a inhalación de gases, humos…</Typography>
+          </td>
+          <td>
+            <Status value="sospecha" />
+          </td>
+          <td>
+            <Typography>2014-12-24 24:15:00</Typography>
+          </td>
+          <td>
+            <Typography weight="bold">ENO</Typography>
+          </td>
+          <td>
+            <Typography weight="bold">GES</Typography>
+          </td>
+          <td>
+            <div class="actions-container">
+              <Buttom variant="text" icon="edit">Editar</Buttom>
+              <Buttom variant="text" color="error" icon="trash">Eliminar</Buttom>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="checkbox" />
+          </td>
+          <td>
+            <Typography
+              variant="p"
+              type="primary"
+            >Afecciones respiratorias debidas a inhalación de gases, humos…</Typography>
+          </td>
+          <td>
+            <Status />
+          </td>
+          <td>
+            <Typography>2014-12-24 24:15:00</Typography>
+          </td>
+          <td>
+            <Typography weight="bold">ENO</Typography>
+          </td>
+          <td>
+            <Typography weight="bold">GES</Typography>
+          </td>
+          <td>
+            <div class="actions-container">
+              <Buttom variant="text" icon="edit">Editar</Buttom>
+              <Buttom variant="text" color="error" icon="trash">Eliminar</Buttom>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
 
-      <Buttom @onClick="mainClick()">Nuevo Diagnóstico</Buttom>
-    </Card>
+    <Buttom @onClick="mainClick()">Nuevo Diagnóstico</Buttom>
+
     <br />
-    <Card align="left">
-      <div class="flex">
-        Typography
-        <Typography variant="h1">text h1 - h6</Typography>
-        <Typography variant="h1" size="16">text h6</Typography>
-        <Typography variant="p">text p</Typography>
-        <Typography variant="p" weight="bold">text bold</Typography>
-        <Typography variant="p" weight="lighter">text lighter</Typography>
-        <Typography variant="p">text normal</Typography>
-        <Typography type="primary">primary color</Typography>
-        <Typography type="error">error color</Typography>
-      </div>
-      <div class="flex">
-        Button
-        <Buttom>Default</Buttom>
-        <Buttom color="error">Error</Buttom>
-        <Buttom variant="text">Default text button</Buttom>
-        <Buttom variant="text" color="error">Error text button</Buttom>
-        <Buttom variant="text" icon="edit">icon edit</Buttom>
-        <Buttom variant="text" color="error" icon="trash">icon trash</Buttom>
-      </div>
-    </Card>
+
+    <div class="flex">
+      Typography
+      <Typography variant="h1">text h1 - h6</Typography>
+      <Typography variant="h1" size="16">text h6</Typography>
+      <Typography variant="p">text p</Typography>
+      <Typography variant="p" weight="bold">text bold</Typography>
+      <Typography variant="p" weight="lighter">text lighter</Typography>
+      <Typography variant="p">text normal</Typography>
+      <Typography type="primary">primary color</Typography>
+      <Typography type="error">error color</Typography>
+    </div>
+    <div class="flex">
+      Button
+      <Buttom>Default</Buttom>
+      <Buttom color="error">Error</Buttom>
+      <Buttom variant="text">Default text button</Buttom>
+      <Buttom variant="text" color="error">Error text button</Buttom>
+      <Buttom variant="text" icon="edit">icon edit</Buttom>
+      <Buttom variant="text" color="error" icon="trash">icon trash</Buttom>
+    </div>
+
     <br />
-    <Card>
-      <Select />
-    </Card>
+
+    <Modal v-if="showModal">
+      <div slot="body">
+        <Select :options="options" />
+
+        <NotificationTag type="exito" />
+        <NotificationTag type="informacion" />
+        <NotificationTag type="alerta" />
+      </div>
+    </Modal>
+    <Buttom @onClick="showModal = true">Default</Buttom>
   </div>
 </template>
 
 <script>
 // import HelloWorld from "./components/HelloWorld.vue";
 import Buttom from "./components/Button.vue";
-import Card from "./components/Card";
+
 import Typography from "./components/Typography";
 import Select from "./components/Select";
 import Status from "./components/Status";
 import Table from "./components/Table";
+import Modal from "./components/Modal";
+import NotificationTag from "./components/NotificationTag";
 
 export default {
   name: "App",
   components: {
-    Card,
     Buttom,
     Typography,
     Select,
     Status,
-    Table
+    Table,
+    Modal,
+    NotificationTag
   },
   methods: {
     mainClick() {
       console.log("este es un main click");
     }
+  },
+  data() {
+    return {
+      showModal: false,
+      options: [
+        { id: 1, name: "Trombosis profunda" },
+        { id: 2, name: "Trombosis mega profunda" },
+        { id: 3, name: "Hiper mega Trombosis" }
+      ]
+    };
   }
 };
 </script>
---
+++ src/components/NotificationTag.vue
@@ -0,0 +1,93 @@
+<template>
+  <div
+    @click="$emit('onClick')"
+    class="container"
+    v-bind:class="{
+      succes: type === 'exito',
+      info: type === 'informacion',
+      warning: type === 'alerta'
+    }"
+  >
+    <div class="messageContainer">
+      <span class="icon">
+        <svg
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fal"
+          data-icon="pencil"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+          class="svg-inline--fa fa-pencil fa-w-16"
+        >
+          <path
+            fill="currentColor"
+            d="M493.255 56.236l-37.49-37.49c-24.993-24.993-65.515-24.994-90.51 0L12.838 371.162.151 485.346c-1.698 15.286 11.22 28.203 26.504 26.504l114.184-12.687 352.417-352.417c24.992-24.994 24.992-65.517-.001-90.51zm-95.196 140.45L174 420.745V386h-48v-48H91.255l224.059-224.059 82.745 82.745zM126.147 468.598l-58.995 6.555-30.305-30.305 6.555-58.995L63.255 366H98v48h48v34.745l-19.853 19.853zm344.48-344.48l-49.941 49.941-82.745-82.745 49.941-49.941c12.505-12.505 32.748-12.507 45.255 0l37.49 37.49c12.506 12.506 12.507 32.747 0 45.255z"
+            class
+          />
+        </svg>
+      </span>
+      <div class="slotMessage">
+        <slot name="message">
+          <p>mensajes</p>
+        </slot>
+      </div>
+    </div>
+
+    <div class="actionContainer">
+      <slot name="action"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotificationTag",
+  props: {
+    type: {
+      type: String,
+      required: false,
+      default: "exito"
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  border-radius: 4px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.container.succes {
+  border: solid 1px #b7eb8f;
+  background-color: #f6ffed;
+}
+
+.container.info {
+  border: solid 1px #91d5ff;
+  background-color: #e6f7ff;
+}
+.container.warning {
+  border: solid 1px #ffe58f;
+  background-color: #fffbe6;
+}
+.slotMessage {
+  display: inline-block;
+}
+.icon {
+  width: 15px;
+  display: inline-block;
+  margin-left: 4px;
+  vertical-align: middle;
+}
+
+/* .slotMessage {
+ 
+} */
+/* .messageContainer {
+  display: inline-block;
+} */
+</style>
